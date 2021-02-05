dc9481cce2ea6be2273579a3e7113a84c50a797c
Martin Grödl <martin@process.studio>
2021-02-05 21:17:51 +0100

    Implemented chapter 14 intructions

--- draftmaster.py
+++ draftmaster.py
@@ -31,7 +31,7 @@ def _join_args(args, sep=','):
         return str(val)
     return sep.join( [stringify(arg_value) for _, arg_value in args] )
 
-# See page TODO
+# See page 14-15
 _hpgl_error_numbers = ['No error', 'Instruction not recognized', 'Wrong number of parameters', 'Out-of range parameter, or illegal character', 'Not used', 'Unknown character set', 'Position overflow (not reported with default E-mask value)', 'Buffer overflow for polygons']
 
 # See page 14-24
@@ -950,97 +950,98 @@ def UC(*increments):
 # Part IV – Interfacing, Handshaking, Output, and Device Control
 # Chapter 14: Obtaining Information from the Plotter
 
-def GC(count_number = None):
+def GC(count_number=None):
+    # See page 14-9
     '''GC, Group Count
     USE: Assigns a number known as the 'group count,' which is the number that will be output by the OG instruction. The group count is an arbitrary number that you assign at the beginning of a data block in spooling applications (typically in an RS-232-C configuration with a mainframe computer). Use this instruction in conjunction with the OG instruction to monitor the successful transfer of data blocks.
-    TODO
     '''
-    if count_number == None:
-        return _cmd('GC;')
-    else:
-        return _cmd(f'GC{count_number};')
+    args = _check_args([
+        [],
+        ['count_number'],
+    ], locals())
+    args = _join_args(args)
+    return _cmd(f'GC{args};')
 
-def IM(E_mask_value = None, S_mask_value = None, P_mask_value = None):
+def IM(e_mask_value, s_mask_value=None, p_mask_value=None):
+    # See page 14-10
     '''IM, Input Mask
     USE: Controls which HP-GL errors are reported. If you are using an HP-IB interface, you can also use IM to control the conditions that cause an HP-IB service reques or a positive response to a parallel poll.
-    TODO
     '''
-    if E_mask_value == None:
-        return _cmd('IM;')
-    elif S_mask_value == None:
-        return _cmd(f'IM{E_mask_value};')
-    elif P_mask_value == None:
-        return _cmd(f'IM{E_mask_value},{S_mask_value};')
-    else:
-        _cmd(f'IM{E_mask_value},{S_mask_value},{P_mask_value};')
+    args = _check_args([
+        ['e_mask_value'],
+        ['e_mask_value', 's_mask_value'],
+        ['e_mask_value', 's_mask_value', 'p_mask_value'],
+    ], locals())
+    args = _join_args(args)
+    return _cmd(f'IM{args};')
 
 def OA():
+    # See page 14-13
     '''OA, Output Actual Pen Status
     USE: Outputs the current pen location (in plotter units) and up/down position. You can use this information to position a label of figure, to determine the parameters of some desired window, or to determine the pen's current location if it has been moved using front-panel Cursor Control buttons.
-    TODO
     '''
     return _cmd('OA;')
 
 def OC():
+    # See page 14-14
     '''OC, Output Commanded Pen Status
     USE: Outputs the location and up/down position of the last commanded move. Use this instruction to position a label or determine the parameters of an instruction that tried to move the pen beyond the limits of some window. You can also use this instruction when you want to know the pen's location in user units. 
-    TODO
     '''
     return _cmd('OC;')
 
 def OE():
+    # See page 14-15
     '''OE, Output Error
     USE: Outputs a number corresponding to the type of HP-GL error (if any) received by the plotter after the most recent IN instruction, front-panel reset, or OE instruction. Use this instruction for debugging programs.
-    TODO
     '''
     return _cmd('OE;')
 
 def OF():
+    # See page 14-17
     '''OF, Output Factors
     USE: Outputs the number of plotter units per millimetre in each axis. This instruction lets you use the plotter with software that needs to know the size of a plotter unit.
-    TODO
     '''
     return _cmd('OF;')
 
 def OG():
+    # See page 14-18
     '''OG, Output Group Count
     USE: Outputs the data block number of the current group count and whether the escape function has been activated. Use this instruction at the end of a data block in spooling applications, where it is important to know the current data block number and whether the data block has been transferred.
-    TODO
     '''
     return _cmd('OG;')
 
 def OI():
+    # See page 14-19
     '''OI, Output Identification
     Outputs the plotter's identifying model number. This information is useful in a remote operating configuration (where several plotters are connected to the computer) to determine which plotter model is on-line, or when software needs the plotter's model number.
-    TODO
     '''
     return _cmd('OI;')
 
 def OL():
+    # See page 14-20
     '''OL, Output Label Length
     Output information about the label contained in the label buffer.
-    TODO
     '''
     return _cmd('OL;')
 
 def OO():
+    # See page 14-22
     '''OO, Output Options
     Outputs eight option parameters indicating features implemented on the plotter. Some software packages use this feature to determine which plotter capabilities exist.
-    TODO
     '''
     return _cmd('OO;')
 
 def OS():
+    # See page 14-23
     '''OS, Output Status
     Outputs the decimal value of the status byte. Use this instruction in debugging operations and in digitizing applications.
-    TODO
     '''
     return _cmd('OS;')
 
 def OT():
+    # See page 14-25
     '''OT, Output Carousel Type
     The OT instruction outputs information on the type of carousel loaded and the stalls occupied.
-    TODO
     '''
     return _cmd('OT;')
 
